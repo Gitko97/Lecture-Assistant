6642dd56fc270e54a7dc63ee122fb793e643bd0c
TelAr <qet112313@gmail.com>
2020-05-16 03:08:32 +0900

    prototype setting
    compare buffered img prototype

--- PDF_compare/src/pdf_compare/Main.java
+++ PDF_compare/src/pdf_compare/Main.java
@@ -1,10 +1,30 @@
+//this is for testing
 package pdf_compare;
 
-//this is for testing
+import java.util.*;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 public class Main {
 	public static void main(String[] args)
 	{
+		File a, b;
+		BufferedImage ba=null, bb=null;
+		a=new File("sampleA.bmp");
+		b=new File("sampleB.bmp");
 		
+		try {
+		ba=ImageIO.read(a);
+		bb=ImageIO.read(b);
+		}
+		catch(Exception e) {};
 		
+		PDFCompare p=new PDFCompare(ba, bb);
+		p.compare();
+		System.out.println(p.show_dif());
+		return;
 	}
 }
--- PDF_compare/src/pdf_compare/PDFCompare.java
+++ PDF_compare/src/pdf_compare/PDFCompare.java
@@ -1,6 +1,92 @@
 package pdf_compare;
+import java.awt.image.BufferedImage;
+
 
 public class PDFCompare {
-	private int dif=0;
+	private long dif;
+	private BufferedImage fromPDF, fromVideo;
+	private int height, width;
 	
+	//initiate
+	//get bufferedimg when initiate
+	public PDFCompare(BufferedImage a, BufferedImage b)
+	{
+		dif=0;
+		fromPDF=a;
+		fromVideo=b;
+		
+		height=fromPDF.getHeight();
+		width=fromPDF.getWidth();
+	}
+	//initiate without bufferedimg
+	public PDFCompare()
+	{
+		dif=0;
+		fromPDF=null;
+		fromVideo=null;
+	}
+	
+	//show dif(just debugging)
+	public long show_dif()
+	{
+		return dif;
+	}
+	public int get_img(BufferedImage a, BufferedImage b)
+	{
+		if(a==null||b==null)
+		{
+			throw new NullPointerException();
+		}
+		fromPDF=a;
+		fromVideo=b;
+		
+		height=fromPDF.getHeight();
+		width=fromPDF.getWidth();
+		return 0;
+	}
+	
+	//convert RGB to dif level
+	public int getRGBdif(int rgb)
+	{
+		int result;
+		int red, grean, blue;
+		
+		red=rgb/(256*256);
+		grean=(rgb%(256*256))/256;
+		blue=rgb%256;
+		
+		result=red+grean+blue;
+		
+		return result;
+	}
+	
+	
+	//compare two bufferedimg, 
+	//if same, return false. else, return true
+	public boolean compare()
+	{
+		int row, cor;
+		int pdfPixel, videoPixel;
+		
+		//exception case if buffered is null
+		if(fromPDF==null||fromVideo==null)
+		{
+			throw new NullPointerException();
+		}
+		
+		for(row=0;row<height;row++)
+		{
+			for(cor=0;cor<width;cor++)
+			{
+				pdfPixel=fromPDF.getRGB(cor, row);
+				videoPixel=fromVideo.getRGB(cor, row);
+				dif+=getRGBdif(pdfPixel>videoPixel ? pdfPixel-videoPixel : videoPixel-pdfPixel);
+			}
+		}
+		
+		return false;
+	}
+	
+
+
 }
