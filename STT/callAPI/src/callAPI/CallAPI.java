bbcd2b8a1a547addd742ef991b8cdd7565342b37
Boazlarry <49400168+Boazlarry@users.noreply.github.com>
2020-05-29 21:55:13 +0900

    STT_intiniteStreamin
    코드 변경중

--- STT/callAPI/src/callAPI/CallAPI.java
+++ STT/callAPI/src/callAPI/CallAPI.java
@@ -1,4 +1,4 @@
-package Sound;
+package callAPI;
 
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.FixedCredentialsProvider;
@@ -15,10 +15,15 @@ import com.google.cloud.speech.v1.StreamingRecognitionResult;
 import com.google.cloud.speech.v1.StreamingRecognizeRequest;
 import com.google.cloud.speech.v1.StreamingRecognizeResponse;
 import com.google.protobuf.ByteString;
+import com.google.protobuf.Duration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -29,8 +34,26 @@ import javax.sound.sampled.TargetDataLine;
 public class CallAPI implements Runnable
 {
 	
+	static final int STREAMING_LIMIT = 290000;
+	
 	static SpeechClient client = null;
 	static boolean exit;
+	static int BYTES_PER_BUFFER = 6400;
+	
+	 private static volatile BlockingQueue<byte[]> sharedQueue = new LinkedBlockingQueue();
+	
+	private static int restartCounter = 0;
+	static ArrayList<ByteString> audioInput = new ArrayList<ByteString>();
+	static ArrayList<ByteString> lastAudioInput = new ArrayList<ByteString>();
+	static int resultEndTimeInMS = 0;
+	static int isFinalEndTime = 0;
+	static int finalRequestEndTime = 0;
+	static boolean newStream = true;
+	static double bridgingOffset = 0;
+	static boolean lastTranscriptWasFinal = false;
+	static StreamController referenceToStreamController;
+	static ByteString tempByteString;
+	
 	
 	public CallAPI(String keyPath) {
 		try {
@@ -45,7 +68,23 @@ public class CallAPI implements Runnable
 		exit = true;
 	}
 	
-	static void authExplicit(String jsonPath) throws IOException  // key/key.json ���� �̿���  Credential ��ü ���� �� �̸� �̿��� SpeechClient ��ü�� �����Ͽ� ����ƽ client�� �Ҵ�
+	static String convertMillisToDate(double milliSeconds)
+	{
+		long millis = (long) milliSeconds;
+	    DecimalFormat format = new DecimalFormat();
+	    format.setMinimumIntegerDigits(2);
+	    return String.format(
+		        "%s:%s /",
+		        format.format(TimeUnit.MILLISECONDS.toMinutes(millis)),
+		        format.format(
+		        		TimeUnit.MILLISECONDS.toSeconds(millis)
+		        		- TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
+		        		)
+		        );
+	}
+
+	
+	static void authExplicit(String jsonPath) throws IOException  // key/key.json 占쏙옙占쏙옙 占싱울옙占쏙옙  Credential 占쏙옙체 占쏙옙占쏙옙 占쏙옙 占싱몌옙 占싱울옙占쏙옙 SpeechClient 占쏙옙체占쏙옙 占쏙옙占쏙옙占싹울옙 占쏙옙占쏙옙틱 client占쏙옙 占쌀댐옙
 	{	
 		CredentialsProvider credentialsProvider = FixedCredentialsProvider.create(ServiceAccountCredentials.fromStream(new FileInputStream(jsonPath)));
 		SpeechSettings settings = SpeechSettings.newBuilder().setCredentialsProvider(credentialsProvider).build();
@@ -55,44 +94,52 @@ public class CallAPI implements Runnable
 	public void run() {
 		ResponseObserver<StreamingRecognizeResponse> responseObserver = null;
 
-		try {
-	        responseObserver =
-	            new ResponseObserver<StreamingRecognizeResponse>() {
-	              //ArrayList<StreamingRecognizeResponse> responses = new ArrayList<>(); 응답 arrayList 주석처리
+		try
+		{
+			responseObserver = new ResponseObserver<StreamingRecognizeResponse>()
+			{
+				ArrayList<StreamingRecognizeResponse> responses = new ArrayList<>();
+				public void onStart(StreamController controller) 
+				{  
+                }
 
-	              public void onStart(StreamController controller) 
-	              {
-	            	  
-	              }
+                public void onResponse(StreamingRecognizeResponse response) // onResponse占쏙옙 response占쏙옙 占쏙옙 占쏙옙 占쏙옙占쏙옙 占쌥뱄옙풔占� 占쌉쇽옙. 占쌩곤옙占쌩곤옙 占쏙옙占쏙옙占쏙옙占� 占쏙옙占쏙옙求占� 占쌘듸옙
+	            {
+	             responses.add(response);      	  
+	             StreamingRecognitionResult result = response.getResultsList().get(0);
+	             Duration resultEndTime = result.getResultEndTime();
+	             resultEndTimeInMS =
+	                 (int) 	((resultEndTime.getSeconds() * 1000) + (resultEndTime.getNanos() / 1000000));
+	              double correctedTime =
+	                  resultEndTimeInMS - bridgingOffset + (STREAMING_LIMIT * restartCounter);
 
-	              public void onResponse(StreamingRecognizeResponse response) // onResponse�� response�� �� �� ���� �ݹ�Ǵ� �Լ�. �߰��߰� ������� ����ϴ� �ڵ�
-	              {
-	            	  StreamingRecognitionResult result = response.getResultsList().get(0);
-	                  SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-	                  System.out.printf("Transcript : %s\n", alternative.getTranscript());
-		              
-	                 // responses.add(response); 응답 arrayList 주석처리
-		              
+	              SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+	              if (result.getIsFinal()) {
+	                System.out.print("\033[2K\r");
+	                System.out.printf(
+	                    "%s: %s [confidence: %.2f]\n",
+	                    convertMillisToDate(correctedTime),
+	                    alternative.getTranscript(),
+	                    alternative.getConfidence());
+	                isFinalEndTime = resultEndTimeInMS;
+	                lastTranscriptWasFinal = true;
+	              } else {
+	                System.out.print("\033[2K\r");
+	                System.out.printf(
+	                    "%s: %s", convertMillisToDate(correctedTime), alternative.getTranscript());
+	                lastTranscriptWasFinal = false;
 	              }
+	            }
 
-	              public void onComplete() 
-	              { 
-	            	  //응답 arrayList 주석처리
-	            	  /*
-	                for (StreamingRecognizeResponse response : responses) 
-	                {
-	                  StreamingRecognitionResult result = response.getResultsList().get(0);
-	                  SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-	                  System.out.printf("Transcript : %s\n", alternative.getTranscript());
-	                }
-	                */
-	              }
+	            public void onComplete() 
+	            { 
+	            }
 
-	              public void onError(Throwable t)
-	              {
-	                System.out.println(t);
-	              }
-	            };
+	            public void onError(Throwable t)
+	            {
+	              System.out.println(t);
+	            }
+			};
 
 	        ClientStream<StreamingRecognizeRequest> clientStream = client.streamingRecognizeCallable().splitCall(responseObserver);
 
@@ -120,7 +167,7 @@ public class CallAPI implements Runnable
 		                audioFormat
 	                ); // Set the system information to read from the microphone audio stream
 
-	        // AudioSystem Ȯ��.
+	        // AudioSystem 확占쏙옙.
 	        if (!AudioSystem.isLineSupported(targetInfo))
 	        {
 	          System.out.println("Microphone not supported");
@@ -132,34 +179,98 @@ public class CallAPI implements Runnable
 	        targetDataLine.open(audioFormat);
 	        targetDataLine.start();
 	        System.out.println("Start speaking");
-	        //long startTime = System.currentTimeMillis();   시간 측정 메소드 일단 주석
+	        
+	        long startTime = System.currentTimeMillis();  // �떆媛� 痢≪젙 硫붿냼�뱶 �씪�떒 二쇱꽍
 	        
 	        // Audio Input Stream
 	        AudioInputStream audio = new AudioInputStream(targetDataLine);
 	        
-	        // ��Ʈ���� ����. ������ �ð��� ������ break
+	        // 占쏙옙트占쏙옙占쏙옙 占쏙옙占쏙옙. 占쏙옙占쏙옙占쏙옙 占시곤옙占쏙옙 占쏙옙占쏙옙占쏙옙 break
 	        while (true)
 	        {
-	         // long estimatedTime = System.currentTimeMillis() - startTime; 시간 측정 메소드 일단 주석 처리
-	          byte[] data = new byte[6400];
-	          audio.read(data);
-	          if (exit) // exit 가 true 일때 까지
-	          { 
-	            System.out.println("Stop speaking.");
-	            targetDataLine.stop();
-	            targetDataLine.close();
-	            break;
-	          }
-	          request =
-	              StreamingRecognizeRequest.newBuilder()
-	                  .setAudioContent(ByteString.copyFrom(data))
-	                  .build();
-	          clientStream.send(request);
+	        	long estimatedTime = System.currentTimeMillis() - startTime;
+
+	            if (estimatedTime >= STREAMING_LIMIT) {
+
+	              clientStream.closeSend();
+	              referenceToStreamController.cancel(); // remove Observer
+
+	              if (resultEndTimeInMS > 0) {
+	                finalRequestEndTime = isFinalEndTime;
+	              }
+	              resultEndTimeInMS = 0;
+
+	              lastAudioInput = null;
+	              lastAudioInput = audioInput;
+	              audioInput = new ArrayList<ByteString>();
+
+	              restartCounter++;
+
+	              if (!lastTranscriptWasFinal) {
+	                System.out.print('\n');
+	              }
+
+	              newStream = true;
+
+	              clientStream = client.streamingRecognizeCallable().splitCall(responseObserver);
+
+	              request =
+	                  StreamingRecognizeRequest.newBuilder()
+	                      .setStreamingConfig(streamingRecognitionConfig)
+	                      .build();
+
+	              System.out.printf("%d: RESTARTING REQUEST\n", restartCounter * STREAMING_LIMIT);
+
+	              startTime = System.currentTimeMillis();
+
+	            } else {
+
+	              if ((newStream) && (lastAudioInput.size() > 0)) {
+	                // if this is the first audio from a new request
+	                // calculate amount of unfinalized audio from last request
+	                // resend the audio to the speech client before incoming audio
+	                double chunkTime = STREAMING_LIMIT / lastAudioInput.size();
+	                // ms length of each chunk in previous request audio arrayList
+	                if (chunkTime != 0) {
+	                  if (bridgingOffset < 0) {
+	                    // bridging Offset accounts for time of resent audio
+	                    // calculated from last request
+	                    bridgingOffset = 0;
+	                  }
+	                  if (bridgingOffset > finalRequestEndTime) {
+	                    bridgingOffset = finalRequestEndTime;
+	                  }
+	                  int chunksFromMS =
+	                      (int) Math.floor((finalRequestEndTime - bridgingOffset) / chunkTime);
+	                  // chunks from MS is number of chunks to resend
+	                  bridgingOffset =
+	                      (int) Math.floor((lastAudioInput.size() - chunksFromMS) * chunkTime);
+	                  // set bridging offset for next request
+	                  for (int i = chunksFromMS; i < lastAudioInput.size(); i++) {
+	                    request =
+	                        StreamingRecognizeRequest.newBuilder()
+	                            .setAudioContent(lastAudioInput.get(i))
+	                            .build();
+	                    clientStream.send(request);
+	                  }
+	                }
+	                newStream = false;
+	              }
+
+	              tempByteString = ByteString.copyFrom(sharedQueue.take());
+
+	              request =
+	                  StreamingRecognizeRequest.newBuilder().setAudioContent(tempByteString).build();
+
+	              audioInput.add(tempByteString);
+	            }
+
+	            clientStream.send(request);
 	        }
 	      } catch (Exception e) {
 	        System.out.println(e);
 	      }
-	      //responseObserver.onComplete(); 응답 arrayList 주석처리
+	      //responseObserver.onComplete(); �쓳�떟 arrayList 二쇱꽍泥섎━
 	    }
 }
 
--- STT/callAPI/src/callAPI/Main.java
+++ STT/callAPI/src/callAPI/Main.java
@@ -1,9 +1,10 @@
-package Sound;
+package callAPI;
 
 public class Main {
 	public static void main(String[] args) {
-		CallAPI callAPI = new CallAPI("C:\\Users\\xcvds\\eclipse-workspace\\SoundRecord\\key\\key.json");
+		CallAPI callAPI = new CallAPI("key\\key.json");
 		Thread API = new Thread(callAPI);
 		API.run();
+		
 	}
 }
