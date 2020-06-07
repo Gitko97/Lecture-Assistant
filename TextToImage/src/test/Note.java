5cd7fe9fda8269704e2b1e87ef31fd2874bd5c9e
Gitko97 <xcvdsf8216@gmail.com>
2020-06-07 18:16:37 +0900

    TextToImage Test Case
    You have to add PDFBox library in src
    0.pdf is sample image for test

---
+++ TextToImage/src/test/Note.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.awt.image.BufferedImage;
+
+public class Note {
+	BufferedImage note;
+	int startIndex;
+	int endIndex;
+	
+	public Note(BufferedImage note, int startIndex, int endIndex) {
+		this.note = note;
+		this.startIndex = startIndex;
+		this.endIndex = endIndex;
+	}
+}
