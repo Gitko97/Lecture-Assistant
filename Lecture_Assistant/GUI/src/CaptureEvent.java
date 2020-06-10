0becb75a9d7248ef0c78bba015c33a6efb099994
DongKyu Kim <aass0903@naver.com>
2020-06-11 00:39:07 +0900

    CaptureEvent.java aplys convention rules

--- Lecture_Assistant/GUI/src/CaptureEvent.java
+++ Lecture_Assistant/GUI/src/CaptureEvent.java
@@ -6,175 +6,180 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.io.IOException;
-
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-
 public class CaptureEvent implements MouseListener, MouseMotionListener {
-	DrawPanel drawpanel;
-	private String pdfPath = null;
-	private String keyPath = null;
-	int beforeX;
-	int beforeY;
-	int curX;
-	int curY;
-	int clickedShape=0;
-	boolean start;
-	CaptureView captureView;
-	LA_controller controller;
-	public class PDFButtonEvent implements ActionListener {
+  DrawPanel drawpanel;
+  private String pdfPath = null;
+  private String keyPath = null;
+  int beforeX;
+  int beforeY;
+  int curX;
+  int curY;
+  int clickedShape=0;
+  boolean start;
+  CaptureView captureView;
+  LA_controller controller;
+  public class PDFButtonEvent implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-		   JFileChooser fc = new JFileChooser("/Users/");
-		   fc.setDialogTitle("Select pdf file");
+    @Override
+	public void actionPerformed(ActionEvent arg0) {
+	  JFileChooser fc = new JFileChooser("/Users/");
+	  fc.setDialogTitle("Select pdf file");
 		   
-		   fc.setFileFilter(new FileNameExtensionFilter("pdf", "pdf")); //.pdf 파일만 선택하도록
-		   fc.setMultiSelectionEnabled(false);                          //다중 선택 불가
+	  fc.setFileFilter(new FileNameExtensionFilter("pdf", "pdf")); //.pdf �뙆�씪留� �꽑�깮�븯�룄濡�
+	  fc.setMultiSelectionEnabled(false);                          //�떎以� �꽑�깮 遺덇�
 
-	       fc.showOpenDialog(null); // showOpenDialog는 창을 띄우는데 어느 위치에 띄울건지 인자를 받고
-	                                                      // 그리고 선택한 파일의 경로값을 반환한다.
+	  fc.showOpenDialog(null); // showOpenDialog�뒗 李쎌쓣 �쓣�슦�뒗�뜲 �뼱�뒓 �쐞移섏뿉 �쓣�슱嫄댁� �씤�옄瑜� 諛쏄퀬
+	                                                      // 洹몃━怨� �꽑�깮�븳 �뙆�씪�쓽 寃쎈줈媛믪쓣 諛섑솚�븳�떎.
 	        
 	        
-	        if(fc.getSelectedFile() != null) {
-	        	pdfPath = fc.getSelectedFile().toString(); 
-	        	try {
-	        		controller.GetLecturePDF(pdfPath);
-	        		captureView.setLabel1(pdfPath);
-	        	} catch (IOException e) {
-	        		pdfPath = null;
-	        		JOptionPane.showMessageDialog(null, "This pdf file cannot be read");
-	        	}
-	        }
-		}
+	  if(fc.getSelectedFile() != null) {
+	    pdfPath = fc.getSelectedFile().toString(); 
+	    try {
+	      controller.GetLecturePDF(pdfPath);
+	      captureView.setLabel1(pdfPath);
+	      } catch (IOException e) {
+	        pdfPath = null;
+	        JOptionPane.showMessageDialog(null, "This pdf file cannot be read");
+	      }
+	    }
+	  }
 		
-	}
+  }
 	
-	public class CloseButtonEvent implements ActionListener {
+  public class CloseButtonEvent implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			captureView.exit();
-		}
+    @Override
+	public void actionPerformed(ActionEvent arg0) {
+	  // TODO Auto-generated method stub
+	  captureView.exit();
+	}
 		
-	}
+  }
 	
-	public class StartButtonEvent implements ActionListener {
+  public class StartButtonEvent implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			if(pdfPath == null || keyPath == null) {
-				JOptionPane.showMessageDialog(null, "You have to input PDF File and Key File");
-			}
-			else { 
-				start = true; 
-				try {
-					controller.start();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					JOptionPane.showMessageDialog(null, "Microphone not support");
-				}
-			}
-		}
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+	  // TODO Auto-generated method stub
+	  if(pdfPath == null || keyPath == null) {
+	    JOptionPane.showMessageDialog(null, "You have to input PDF File and Key File");
+	  } else { 
+		start = true; 
+		try {
+		  controller.start();
+		  } catch (InterruptedException e) {
+		    // TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, "Microphone not support");
+		  }
+	  }
+	}
 		
-	}
+  }
 	
-	public class ExitButtonEvent implements ActionListener {
+  public class ExitButtonEvent implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			if(start)
-			 if(controller.exit()) 
-				 {
-				 	JOptionPane.showMessageDialog(null, "Save Complete!");
-				 	start = false;
-				 	controller = new LA_controller(captureView);
-				 }
+    @Override
+	public void actionPerformed(ActionEvent arg0) {
+	  // TODO Auto-generated method stub
+	  if(start) {
+	    if(controller.exit()) {
+		  JOptionPane.showMessageDialog(null, "Save Complete!");
+		  start = false;
+		  controller = new LA_controller(captureView);
 		}
+	  }
+    }
 		
-	}
+  }
 	
-	public class KeyButtonEvent implements ActionListener {
+  public class KeyButtonEvent implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			JFileChooser fc = new JFileChooser("/Users/");
-			fc.setDialogTitle("Select Key file");
-			fc.setFileFilter(new FileNameExtensionFilter("json", "json")); //.pdf 파일만 선택하도록
-			fc.setMultiSelectionEnabled(false);                          //다중 선택 불가
-	        fc.showOpenDialog(null); // showOpenDialog는 창을 띄우는데 어느 위치에 띄울건지 인자를 받고
-	                                                      // 그리고 선택한 파일의 경로값을 반환한다.
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+	  // TODO Auto-generated method stub
+      JFileChooser fc = new JFileChooser("/Users/");
+	  fc.setDialogTitle("Select Key file");
+	  fc.setFileFilter(new FileNameExtensionFilter("json", "json")); //.pdf �뙆�씪留� �꽑�깮�븯�룄濡�
+	  fc.setMultiSelectionEnabled(false);                          //�떎以� �꽑�깮 遺덇�
+	  fc.showOpenDialog(null); // showOpenDialog�뒗 李쎌쓣 �쓣�슦�뒗�뜲 �뼱�뒓 �쐞移섏뿉 �쓣�슱嫄댁� �씤�옄瑜� 諛쏄퀬
+	                                                      // 洹몃━怨� �꽑�깮�븳 �뙆�씪�쓽 寃쎈줈媛믪쓣 諛섑솚�븳�떎.
 
-	        if(fc.getSelectedFile() != null)  {
-	        	String []language = {"한국어", "English"};
-	        	int selected = JOptionPane.showOptionDialog(null, "Language", "The language is the lecture in",
-	        	  JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,language, language[0]);
-	        	if(selected != JOptionPane.CLOSED_OPTION) {
-	        		keyPath = fc.getSelectedFile().toString();
-	        		try {
-	        			controller.Authentication(keyPath,langCode(language[selected]));
-	        			captureView.setLabel2(keyPath);
-	        		} catch (IOException e) {
-	        			keyPath = null;
-	        			JOptionPane.showMessageDialog(null, "This key file does not work.");
-	        		}
-	        	}
-	        }
-		}
+	  if(fc.getSelectedFile() != null) {
+	    String []language = {"�븳援��뼱", "English"};
+	    int selected = JOptionPane.showOptionDialog(null, "Language", "The language is the lecture in",
+	     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,language, language[0]);
+	    if(selected != JOptionPane.CLOSED_OPTION) {
+	      keyPath = fc.getSelectedFile().toString();
+	      try {
+	        controller.Authentication(keyPath,langCode(language[selected]));
+	        captureView.setLabel2(keyPath);
+	      } catch (IOException e) {
+	        keyPath = null;
+	        JOptionPane.showMessageDialog(null, "This key file does not work.");
+	      }
+	    }
+	  }
+    }
 		
-		public String langCode(String lang) {
-			switch(lang) {
-				case "한국어": return "ko-KR";
-				case "English" : return "en-US";
-			}
-			return "en-US";
-		}
+	public String langCode(String lang) {
+	  switch(lang) {
+	    case "�븳援��뼱": return "ko-KR";
+		case "English" : return "en-US";
+	  }
+	  return "en-US";
 	}
+  }
 	
-	public CaptureEvent(CaptureView captureView) {
-		controller = new LA_controller(captureView);
-		this.captureView = captureView;
-		start = false;
-	}
+  public CaptureEvent(CaptureView captureView) {
+    controller = new LA_controller(captureView);
+	this.captureView = captureView;
+	start = false;
+  }
 
-	@Override
-	public void mouseDragged(MouseEvent e) {	// 마우스 드레그 이벤트
-		curX = e.getX();
-		curY = e.getY();
+  @Override
+  public void mouseDragged(MouseEvent e) {	// 留덉슦�뒪 �뱶�젅洹� �씠踰ㅽ듃
+    curX = e.getX();
+    curY = e.getY();
 		
-		if(clickedShape == 0) captureView.MovePos(curX-beforeX, curY-beforeY);	//검은색 네모 눌렀으면 드래그시 이동
-		else if(clickedShape == -1) captureView.MovePanel(curX-beforeX, curY-beforeY);
-		else captureView.Resize(curX-beforeX, curY-beforeY, clickedShape);		// 초록색 네모 눌렀으면 드래그시 resize
-		beforeX = curX;
-		beforeY = curY;
+	if(clickedShape == 0) {
+	  captureView.MovePos(curX-beforeX, curY-beforeY);	//寃����깋 �꽕紐� �닃���쑝硫� �뱶�옒洹몄떆 �씠�룞
+	} else if(clickedShape == -1) {
+	  captureView.MovePanel(curX-beforeX, curY-beforeY);
+	} else {
+	  captureView.Resize(curX-beforeX, curY-beforeY, clickedShape);		// 珥덈줉�깋 �꽕紐� �닃���쑝硫� �뱶�옒洹몄떆 resize
 	}
+	beforeX = curX;
+	beforeY = curY;
+  }
 
-	@Override
-	public void mousePressed(MouseEvent e) {	// 마우스 누를때 이벤트
-		beforeX = e.getX();
-		beforeY = e.getY();	
-		this.clickedShape = captureView.Clicked(beforeX,beforeY);	// 무엇을 눌렀는지 int로 저장
-		if(clickedShape == 0) captureView.setCursor(new Cursor(Cursor.MOVE_CURSOR));
-		else if(clickedShape == -1) captureView.setCursor(new Cursor(Cursor.MOVE_CURSOR));
-		else  captureView.setCursor(new Cursor(Cursor.HAND_CURSOR));
+  @Override
+  public void mousePressed(MouseEvent e) {	// 留덉슦�뒪 �늻瑜쇰븣 �씠踰ㅽ듃
+    beforeX = e.getX();
+	beforeY = e.getY();	
+	this.clickedShape = captureView.Clicked(beforeX,beforeY);	// 臾댁뾿�쓣 �닃���뒗吏� int濡� ���옣
+	if(clickedShape == 0) {
+	  captureView.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+	} else if(clickedShape == -1) {
+	  captureView.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+	} else {
+	  captureView.setCursor(new Cursor(Cursor.HAND_CURSOR));
 	}
+  }
 	
-	@Override
-	public void mouseMoved(MouseEvent e) {}
-	public void mouseClicked(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {
-		captureView.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-	}
+  @Override
+  public void mouseMoved(MouseEvent e) {}
+  public void mouseClicked(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {}
+  public void mouseReleased(MouseEvent e) {
+    captureView.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+  }
 
-	public Thread getThread() {
-		return Thread.currentThread();
-	}
+  public Thread getThread() {
+    return Thread.currentThread();
+  }
 }
