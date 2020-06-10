e0571839265e48ba664e2314e21de10e113dd00c
DongKyu Kim <aass0903@naver.com>
2020-06-11 02:30:20 +0900

    PDFandIMG.java applys convention rules

--- Lecture_Assistant/PDFandImage/src/PDFandIMG.java
+++ Lecture_Assistant/PDFandImage/src/PDFandIMG.java
@@ -16,35 +16,36 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class PDFandIMG{
 	
-	public boolean IMGtoPDF(ArrayList<BufferedImage> imgs, String filePath) {	// 매개변수로 받은 imgs들을 filePath에 저장
-		PDDocument document = new PDDocument();
-		try {
-			for(BufferedImage img : imgs) {
-				PDPage page = new PDPage(new PDRectangle(img.getWidth(), img.getHeight()));
-				document.addPage(page);
-				PDImageXObject pdImage = JPEGFactory.createFromImage(document, img);
-				PDPageContentStream contentStream = new PDPageContentStream(document, page);
-				contentStream.drawImage(pdImage, 0, 0, img.getWidth(), img.getHeight());
-				contentStream.close();
-			}
-			document.save(filePath);
-			return true;
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
+  public boolean IMGtoPDF(ArrayList<BufferedImage> imgs, String filePath) {	// 留ㅺ컻蹂��닔濡� 諛쏆� imgs�뱾�쓣 filePath�뿉 ���옣
+    PDDocument document = new PDDocument();
+	try {
+	  for(BufferedImage img : imgs) {
+	    PDPage page = new PDPage(new PDRectangle(img.getWidth(), img.getHeight()));
+		document.addPage(page);
+		PDImageXObject pdImage = JPEGFactory.createFromImage(document, img);
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+		contentStream.drawImage(pdImage, 0, 0, img.getWidth(), img.getHeight());
+		contentStream.close();
+	  }
+	  document.save(filePath);
+	  return true;
+	} catch (IOException e) {
+	  e.printStackTrace();
 	}
+	return false;
+  }
 	
-	public ArrayList<BufferedImage> PDFtoIMG(String filePath) throws IOException { //매개변수로 받은 filePath의 PDF를 IMG로 변환후 ArrayList로 반환
-		ArrayList<BufferedImage> imgs = new ArrayList<>();
-		File readFile = new File(filePath);
-			PDDocument document = PDDocument.load(readFile);
-			int pageCount = document.getNumberOfPages();
-			PDFRenderer pdfRenderer = new PDFRenderer(document);
-			for(int i=0;i<pageCount;i++) {
-				imgs.add(pdfRenderer.renderImageWithDPI(i, 100, ImageType.RGB));
-			}
-			document.close();
-		return imgs;
+  public ArrayList<BufferedImage> PdfToImg(String filePath) throws IOException { //留ㅺ컻蹂��닔濡� 諛쏆� filePath�쓽 PDF瑜� IMG濡� 蹂��솚�썑 ArrayList濡� 諛섑솚
+    ArrayList<BufferedImage> imgs = new ArrayList<>();
+	File readFile = new File(filePath);
+	PDDocument document = PDDocument.load(readFile);
+	int pageCount = document.getNumberOfPages();
+	PDFRenderer pdfRenderer = new PDFRenderer(document);
+	
+	for(int i=0; i<pageCount; i++) {
+	  imgs.add(pdfRenderer.renderImageWithDPI(i, 100, ImageType.RGB));
 	}
+	document.close();
+	return imgs;
+  }
 }
