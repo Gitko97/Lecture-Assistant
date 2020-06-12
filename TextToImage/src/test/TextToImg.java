c7b673f3cfe066749f5fe93db756410460c6ef90
Boazlarry <49400168+Boazlarry@users.noreply.github.com>
2020-06-13 00:46:14 +0900

    TTI
    note별로 문단 시작에 표시

--- TextToImage/src/test/TextToImg.java
+++ TextToImage/src/test/TextToImg.java
@@ -21,7 +21,7 @@ public class TextToImg {
 	private ArrayList<Integer> changedP;
 	private int width;
 	private int height;
-	
+	private int pdfPages = 1;
 	private int outputCount = 1;
 	private int fontSize = 20;
 	private Font font;
@@ -31,6 +31,7 @@ public class TextToImg {
 	private Graphics2D graphics;
 	private BufferedImage bImg;
 
+	private String headLine = "";
 	
 	public TextToImg(ArrayList<String> sttString, ArrayList<Note> notes, ArrayList<Integer> changedPosition, int width, int height) throws Exception {
 		this.string = sttString;
@@ -53,7 +54,7 @@ public class TextToImg {
 		int cPosIndex = 0;
 		int widthMargin = width;
 		int heightMargin = 0;
-		
+
 		boolean notePrinted = false;
 		
 		Note note = notes.get(noteIndex++);
@@ -65,22 +66,36 @@ public class TextToImg {
 			
 			if (changedP.get(cPosIndex) == i) {
 				if (cPosIndex < changedP.size() - 1 ) cPosIndex += 1;
+				pdfPages += 1;
 				imageWrite();
 				initGraphic();
 				lineStart = fontSize * 3;
 				wordStart = 10;
 				widthMargin = width;
 				heightMargin = 0;
+				
+				
 				//System.out.println("페이지 넘김");
 			}
 			
 			// ---------------------- 노트 출력 --------------------------------
 			// note의 startIndex과 string의 index가 같다면 같은 초이므로 노트의 이미지를 그릴 준비.
 			if (note.startIndex == i ) {
+				wordStart = 10;
+				if (lineStart + fontSize * 6 > height ) {	// 하단 여백이 모자랄 경우 다음 장으로 넘기기
+					imageWrite();
+					initGraphic();
+					lineStart = fontSize * 3;
+				}
+				else lineStart += fontSize * 2;
 				notePrinted = true;
 				noteImg = note.note;
-				//System.out.println(noteIndex + "번째 그림과 " + i + "초의 텍스트 그리기");
+				headLine = "<"+secondToMinute(i)+"'s note>";
+				graphics.drawString(headLine, wordStart, lineStart);
+				wordStart += headLine.length() * (fontSize - 3);
 			}
+				
+				//System.out.println(noteIndex + "번째 그림과 " + i + "초의 텍스트 그리기");
 			
 			if (noteImg != null) {
 				//System.out.println("그림");
@@ -94,6 +109,7 @@ public class TextToImg {
 					wordStart = 10;
 					lineStart = fontSize * 3;
 				}
+				
 				graphics.drawImage(noteImg, width - noteImg.getWidth() - 5, lineStart, null);
 				widthMargin = width - noteImg.getWidth() - 5;
 				heightMargin = lineStart + noteImg.getHeight();
@@ -103,6 +119,7 @@ public class TextToImg {
 			
 			if (notePrinted) {
 				if (note.endIndex == i) {
+					
 					notePrinted = false;
 					lineStart = heightMargin + fontSize * 3;
 					wordStart = 10;
@@ -118,7 +135,7 @@ public class TextToImg {
 			// i초의 word이다. 
 			if (wordStart + word.length() * (fontSize - 4) + fontSize> widthMargin) {	// 가로의 남은 공간이 불충분하여 줄 띄움
 				wordStart = 10;
-				if (lineStart > height - fontSize * 3){	// 줄 띄울만한 여백이 존재하지 않아 다음 장으로 넘어감
+				if (lineStart > height - fontSize * 2){	// 줄 띄울만한 여백이 존재하지 않아 다음 장으로 넘어감
 					imageWrite();
 					initGraphic();
 					lineStart = fontSize;
@@ -128,7 +145,7 @@ public class TextToImg {
 			word = " " + spaceRemover(word);
 			//System.out.println("wordStart = " + wordStart + " | lineStart = " + lineStart);
 			//System.out.println(word);
-			graphics.drawString(word, wordStart, lineStart);
+			graphics.drawString(word,wordStart, lineStart);
 			wordStart += word.length() * (fontSize - 3);
 			
 		}
@@ -152,7 +169,9 @@ public class TextToImg {
 		graphics.setColor(Color.BLACK);
 		
 		// 위쪽 여백 PDf 인덱스
-		graphics.drawString(Integer.toString(outputCount),width/2, 20);
+		graphics.drawString(Integer.toString(outputCount)+"th page",width-100, 20);
+		graphics.drawString("PDF "+Integer.toString(pdfPages)+"th page",width/2, 20);
+
 	}
 	
 	private void imageWrite() throws IOException {
@@ -160,12 +179,16 @@ public class TextToImg {
 		System.out.println(outputCount++ + "th note converted");
 		fileName = Integer.toString(outputCount)+".png";
 		
-		
-		
 	}
 	private String spaceRemover(String s) {
 		if (s.charAt(0) != ' ') return s;
 		else  s = spaceRemover(s.substring(1));
 		return s;
+	}
+	
+	private String secondToMinute(int s) {
+		String m = Integer.toString(s / 60);
+		String sc = Integer.toString(s % 60);
+		return m+"min "+sc+"sec";
 	}
 }
