11772b2aeb48369a17dead4c0f1651729ec9f05e
Boazlarry <49400168+Boazlarry@users.noreply.github.com>
2020-06-13 18:50:11 +0900

    TTI_Remarks
    주석을 영어로 바꾸고, 가이드에 맞게 고침

--- TextToImage/src/test/Note.java
+++ TextToImage/src/test/Note.java
@@ -2,6 +2,8 @@ package test;
 
 import java.awt.image.BufferedImage;
 
+//Class Note
+//1. args are BufferredImage note, int startIndex, int endIndex
 public class Note {
 	BufferedImage note;
 	int startIndex;
