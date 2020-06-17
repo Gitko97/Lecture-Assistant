d9f836ad57825251f9e07c048a1bf6dcfc27006c
DongKyu Kim <aass0903@naver.com>
2020-06-17 17:11:29 +0900

    Apply convention rules to PDFandIMG.java

--- TTI/src/textToImage/PDFandIMG.java
+++ TTI/src/textToImage/PDFandIMG.java
@@ -3,7 +3,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -18,36 +17,35 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 //2. method IMGtoPDF return boolean : save imgs from args at filePath
 // 3. method PDFtoIMG return ArrayList<BufferedImage> : Convert PDF at filePath from args into ArrayList<imgs>
 public class PDFandIMG{
-	
-	public boolean IMGtoPDF(ArrayList<BufferedImage> imgs, String filePath) {	// save imgs from args at filePath
-		PDDocument document = new PDDocument();
-		try {
-			for(BufferedImage img : imgs) {
-				PDPage page = new PDPage(new PDRectangle(img.getWidth(), img.getHeight()));
-				document.addPage(page);
-				PDImageXObject pdImage = JPEGFactory.createFromImage(document, img);
-				PDPageContentStream contentStream = new PDPageContentStream(document, page);
-				contentStream.drawImage(pdImage, 0, 0, img.getWidth(), img.getHeight());
-				contentStream.close();
-			}
-			document.save(filePath);
-			return true;
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
+  public boolean IMGtoPDF(ArrayList<BufferedImage> imgs, String filePath) {	// save imgs from args at filePath
+    PDDocument document = new PDDocument();
+	try {
+	  for(BufferedImage img : imgs) {
+		PDPage page = new PDPage(new PDRectangle(img.getWidth(), img.getHeight()));
+		document.addPage(page);
+		PDImageXObject pdImage = JPEGFactory.createFromImage(document, img);
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
+		contentStream.drawImage(pdImage, 0, 0, img.getWidth(), img.getHeight());
+		contentStream.close();
+	  }
+	  document.save(filePath);
+	  return true;
+	} catch (IOException e) {
+	  e.printStackTrace();
 	}
+	return false;
+  }
 	
-	public ArrayList<BufferedImage> PDFtoIMG(String filePath) throws IOException { // Convert PDF at filePath from args into ArrayList<imgs>
-		ArrayList<BufferedImage> imgs = new ArrayList<>();
-		File readFile = new File(filePath);
-			PDDocument document = PDDocument.load(readFile);
-			int pageCount = document.getNumberOfPages();
-			PDFRenderer pdfRenderer = new PDFRenderer(document);
-			for(int i=0;i<pageCount;i++) {
-				imgs.add(pdfRenderer.renderImageWithDPI(i, 100, ImageType.RGB));
-			}
-			document.close();
-		return imgs;
+  public ArrayList<BufferedImage> PDFtoIMG(String filePath) throws IOException { // Convert PDF at filePath from args into ArrayList<imgs>
+    ArrayList<BufferedImage> imgs = new ArrayList<>();
+	File readFile = new File(filePath);
+	PDDocument document = PDDocument.load(readFile);
+	int pageCount = document.getNumberOfPages();
+	PDFRenderer pdfRenderer = new PDFRenderer(document);
+	for(int i = 0; i < pageCount; i++) {
+	  imgs.add(pdfRenderer.renderImageWithDPI(i, 100, ImageType.RGB));
 	}
+	document.close();
+	return imgs;
+  }
 }
